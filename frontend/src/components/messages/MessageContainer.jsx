975a46beca98b207472059b90c28154e207dbbf5
shubhangNarain <snvishere@gmail.com>
2024-05-03 23:51:09 +0530

    UI done, moving on to functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,39 @@
-import Messages from "./Messages"
-import MessageInput from "./MessageInput"
+import { TiMessages } from "react-icons/ti";
+
+import Messages from "./Messages";
+import MessageInput from "./MessageInput";
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className="md:min-w-[450px] flex flex-col">
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
         <>
-        <div className="bg-black px-4 py-2 mb-2">
+          <div className="bg-black px-4 py-2 mb-2">
             <span className="label-text font-bold">To:</span>{" "}
             <span className="text-gray-300 font-bold">John Doe</span>
-        </div>
+          </div>
 
-        <Messages/>
-        <MessageInput/>
+          <Messages />
+          <MessageInput />
         </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MessageContainer
+export default MessageContainer;
+
+const NoChatSelected = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-black font-semibold flex flex-col items-center gap-2">
+        <p>Welcome &#128075; John Doe</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,20 +1,23 @@
-import { BsSend} from 'react-icons/bs'
+import { BsSend } from "react-icons/bs";
 
 const MessageInput = () => {
   return (
     <form className="px-4 my-3">
-        <div className="w-full">
-            <input
-                type="text"
-                className="border text-sm rounded-lg block w-full p-2.5 bg-blackgray-700 border-gray-600 text-white"
-                placeholder="Send a Message"
-            />
-            <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3">
-                <BsSend/>
-            </button>
-        </div>
+      <div className="w-full relative">
+        <input
+          type="text"
+          className="border text-sm rounded-lg block w-full p-2.5 bg-blackgray-700 border-gray-600 text-white"
+          placeholder="Send a Message"
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 end-0 flex items-center pe-3"
+        >
+          <BsSend />
+        </button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default MessageInput
+export default MessageInput;
